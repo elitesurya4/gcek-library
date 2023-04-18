3d776344d24c56db881e0a563e5fd4993d69f2df
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2023-04-19 03:25:56 +0530

    main

--- CMS/connection.php
+++ CMS/connection.php
@@ -1,10 +1,21 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$librarygcek_cms = "librarygcek_cms";
+$librarygcek_suggestbooks = "librarygcek_suggestbooks";
+$librarygcek_opacmanual = "librarygcek_opacmanual";
+$librarygcek_noticeboard = "librarygcek_noticeboard";
+$librarygcek_scrollingnotice = "librarygcek_scrollingnotice";
 
-    $con=mysqli_connect("localhost","root","","librarygcek_cms");
-    $con1=mysqli_connect("localhost","root","","librarygcek_suggestbooks");
-    $con2=mysqli_connect("localhost","root","","librarygcek_opacmanual");
-    $con3=mysqli_connect("localhost","root","","librarygcek_noticeboard");
-    $con4=mysqli_connect("localhost","root","","librarygcek_scrollingnotice");
+
+
+
+    $con=mysqli_connect($servername,$username,$password,$librarygcek_cms);
+    $con1=mysqli_connect($servername,$username,$password,$librarygcek_suggestbooks);
+    $con2=mysqli_connect($servername,$username,$password,$librarygcek_opacmanual);
+    $con3=mysqli_connect($servername,$username,$password,$librarygcek_noticeboard);
+    $con4=mysqli_connect($servername,$username,$password,$librarygcek_scrollingnotice);
 
     if(mysqli_connect_error()){
         echo"<script>alert('cannot connect to the database');</script>";
